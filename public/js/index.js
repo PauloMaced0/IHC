b4e804ae803a53eb22531b0d49ad28848a294975
Gabriel Janicas <gabrieljanicas@ua.pt>
2024-05-13 19:15:51 +0100

    change

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+function inprocess() {
+	window.location = "inprocess.html";
+}
+function mywishes() {
+	window.location = "mywishes.html";
+}
+function mybooks() {
+	window.location = "mybooks.html";
+}
+function incomingrequests() {
+	window.location = "incomingrequests.html";
+}
+
+function addnew() {
+	window.location = "addnew.html";
+}
+
+let popup = document.getElementById('popup');
+    function openPopup() {
+	    popup.classList.add('open-popup');
+    }
