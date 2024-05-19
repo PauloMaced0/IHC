2e7c0edf7165b8a314061d21fdabfb06bae8bf36
PauloMaced0 <paulomacedo@ua.pt>
2024-05-19 15:57:07 +0100

    highlight scroll need for home page

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,21 @@
+const scrollIndicator = document.getElementById('scrollIndicator');
+
+if (scrollIndicator) {
+	window.addEventListener('scroll', function() {
+	var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+	if (scrollPosition > 100) {
+		scrollIndicator.style.opacity = '0';
+		scrollIndicator.style.transition = 'opacity 0.8s linear';
+	} else {
+		scrollIndicator.style.opacity = 1;
+		scrollIndicator.style.display = 'flex'; // Make sure to use the initial display type of the element
+	}
+	});
+} else {
+	console.error('Scroll indicator element not found');
+}
+
 document.getElementById('triggerBookModal').addEventListener('click', function () {
     document.getElementById('bookDescriptionModal').classList.remove('hidden');
 });
